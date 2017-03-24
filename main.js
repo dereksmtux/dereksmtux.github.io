37c3ad90d62825ea2d37fccfe371851254ec45ac
dereksmtux <dereksmith2112@verizon.net>
2017-03-24 09:50:24 -0500

    further progress on reactiveness

--- main.js
+++ main.js
@@ -4,15 +4,28 @@ function main(){
   $('.back-button').hide();
   $('.main').fadeIn(1500);
   //on click for gaming icon
-  $(".left").on('click',  function() {
+  $("#gaming-icon").on('click',  function() {
     //resizes element
     $('.left').width('70%');
     $('#gaming-icon').width('25%')
-    //gets rid of the left element TO DO 
+    //gets rid of the left element TO DO
     $('.right').toggle();
     $('.back-button').toggle();
 
   });
+// backbutton event handler
+$('.back-button').on('click', function() {
+  changeSize('.left', '25%', '85%');
+  $('.right').toggle();
+  $('.back-button').toggle();
+});
+}
+
+//function for resizing
+function changeSize(tag, size, iconSize) {
+
+  $( tag ).width( size );
+  $('#gaming-icon').width(iconSize);
 
 }
 
