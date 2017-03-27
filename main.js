89554ce0ad18a1e847d6aa58682aab0459b62f4f
dereksmtux <dereksmith2112@verizon.net>
2017-03-27 17:44:41 -0500

    visual changes

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $('.choice').on('click', function(){
   $(this).parent().toggle();
   $(this).parent().toggleClass('clicked', 300);
   $(this).toggleClass('clicked')
-  $('.content').slideToggle(500);
+  $('.content').slideToggle();
   $('.back-button').toggle();
 });
 
